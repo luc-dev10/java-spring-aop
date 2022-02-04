66ae7256e29cf20605e902cafaa9cd7a05091ad3
luc-dev10 <luc-dev1008@gmail.com>
2022-02-04 17:35:55 -0500

    Add Service

---
+++ src/main/java/com/aop/service/AccountService.java
@@ -0,0 +1,19 @@
+package com.aop.service;
+
+import com.aop.entity.Account;
+
+import java.util.List;
+
+public interface AccountService {
+
+    List<Account> getAccounts();
+
+    void saveAccount(Account account);
+
+    Account getAccount(int id);
+
+    void deleteAccount(Account account);
+
+    boolean isServiceRunning();
+
+}

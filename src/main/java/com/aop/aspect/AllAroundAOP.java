3acef6a433326ea15233a1dfbdda000f30bbfe75
luc-dev10 <lucio.dev1008@gmail.com>
2022-02-05 08:54:28 -0500

    add all around aop

--- src/main/java/com/aop/aspect/AllAroundAOP.java
+++ src/main/java/com/aop/aspect/AllAroundAOP.java
@@ -1,17 +1,35 @@
 package com.aop.aspect;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class AllAroundAOP {
 
-    @Around("execution(* com.aop.dao.saveAccount(..))")
-    public void trackMethodAllAround(JoinPoint joinPoint) {
+    // logger
+    private final Logger logger = Logger.getLogger(getClass().getName());
 
+    @Around("execution(* com.aop.dao.AccountDAO.saveAccount(..))")
+    public Object trackMethodAllAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        // start
+        double begin = System.currentTimeMillis();
+
+        Object result = proceedingJoinPoint.proceed();
+
+        // calculate execution time
+        double end = System.currentTimeMillis();
+        double duration = (end - begin) / 1000;
+
+        // print with format
+        logger.info(String.valueOf(duration));
+
+        return result;
     }
 
 }
